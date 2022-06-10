3eccb55a0d69ba42d85348e6d3631c5ec2c1d306
Evan.Jiao <quert365@gmail.com>
2022-06-10 23:09:04 +1000

    Ok Last Push for SET 2

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -18,18 +18,20 @@ print("hello! Let's get started")  # it printed "hello! Let's get started"
 # Creates a list with some words in it. 
 some_words = ["what", "does", "this", "line", "do", "?"]
 
+#Loops each item from the list some_words and prints them 
 for word in some_words:
     print(word)
-
+#Same thing as above
 for x in some_words:
     print(x)
-
+#Prints the list some_words
 print(some_words)
 
+#If statement to check if the list length is bigger than 3and prints the following statement
 if len(some_words) > 3:
     print("some_words contains more than 3 words")
 
-
+#UsefulFunction prints out information regarding the current operating system the function uname() was run on.
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
